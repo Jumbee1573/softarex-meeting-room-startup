d15ee04f6301e2b88304e56aa16b6035036f9bc7
Jumbee1573 <stasik1999box@gmail.com>
2019-11-18 15:25:19 +0300

    creating project architecture

---
+++ src/store.js
@@ -0,0 +1,16 @@
+import { createStore, compose, applyMiddleware } from "redux";
+import rootReducer from "./reducers/index";
+
+const composeEnhancers =
+  process.env.NODE_ENV !== "production" &&
+  typeof window === "object" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const configureStore = preloadedState =>
+  createStore(rootReducer, preloadedState, composeEnhancers(applyMiddleware()));
+
+const store = configureStore({});
+
+export default store;
